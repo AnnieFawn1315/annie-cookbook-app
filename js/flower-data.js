d992d944eba6ee7c88d5b8529b35ea481bd9d1ee
Annie Phan <50613472+AnnieFawn@users.noreply.github.com>
2020-08-30 19:43:27 -0700

    Initial commit

---
+++ js/flower-data.js
@@ -0,0 +1,13 @@
+var flowerData = [
+    { title:'Pound Cake', path:'./img/recipes/recipes-01-md.jpg', desc:'Jelly dessert pudding donut cake cheesecake oat cake. Pastry icing lollipop. Gummi bears topping pie jelly.'},
+    { title:'Chiffon Cake', path:'./img/recipes/recipes-02-md.jpg', desc:'Lemon drops lollipop pie marzipan tootsie roll bonbon marshmallow candy gummies. Dragée tart cake bear claw. Cheesecake gingerbread pie macaroon. Oat cake chupa chups toffee donut sugar plum liquorice.'},
+    { title:'Chocolate Mousse', path:'./img/recipes/recipes-03-md.jpg', desc:'Sweet cake chocolate tart gummies. Topping gummi bears dessert chocolate. Gingerbread oat cake cake apple pie cotton candy dessert. Candy canes liquorice sesame snaps pastry caramels candy canes oat cake.'},
+    { title:'Chocolate Chip Cookies', path:'./img/recipes/recipes-04-md.jpg', desc:'Cake brownie topping. Marzipan liquorice caramels. Marshmallow toffee halvah jujubes powder.'},
+    { title:'Carrot Cake', path:'./img/recipes/recipes-05-md.jpg', desc:'Sweet cake chocolate tart gummies. Topping gummi bears dessert chocolate. Gingerbread oat cake cake apple pie cotton candy dessert. Candy canes liquorice sesame snaps pastry caramels candy canes oat cake.'},
+    { title:'Chocolate Oatmeal Cookies', path:'./img/recipes/recipes-06-md.jpg', desc:'Macaroon gummi bears cookie marshmallow bear claw caramels sweet cotton candy. Muffin gingerbread chocolate cake cupcake muffin topping cookie donut. Carrot cake wafer icing cupcake.'},
+    { title:'Chocolate Fudge', path:'./img/recipes/recipes-07-md.jpg', desc:'Lorem ipsum dolor sit amet'},
+    { title:'Strawberry Short Cake', path:'./img/recipes/recipes-08-md.jpg', desc:'Pellentesque interdum lacus eget ligula tempus'},
+    { title:'Red Velvet Cake', path:'./img/recipes/recipes-09-md.jpg', desc:'Marzipan sweet roll cheesecake icing cheesecake marshmallow tiramisu sugar plum. Carrot cake toffee carrot cake candy canes bear claw. Sweet candy canes topping pastry bonbon apple pie.'},
+    { title:'Coffee Macarons', path:'./img/recipes/recipes-10-md.jpg', desc:'Bonbon halvah powder bear claw bear claw tootsie roll. Jujubes halvah donut bonbon macaroon sweet marzipan pie sesame snaps. Sugar plum bear claw candy canes chupa chups marshmallow sesame snaps halvah chocolate apple pie.'},
+
+];
